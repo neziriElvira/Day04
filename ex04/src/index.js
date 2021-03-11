7028f58674aa835a8dd9309e3891b42f6d27ea7d
Elvira Neziri <neziiri.elvira@gmail.com>
2021-03-11 23:25:38 +0100

    Added all exercises for 4th day

---
+++ ex04/src/index.js
@@ -0,0 +1,16 @@
+function main(numOne, numTwo) {
+
+    //Only change code below this line
+    var myDecimal = 8.2;
+    var myDecimalE = 254e-5;
+    //Only change code above this line
+
+    return {
+        myDecimal,
+        myDecimalE
+    };
+}
+
+console.log(main());
+
+module.exports = main;
---
+++ ex10/src/index.js
@@ -0,0 +1,19 @@
+function main() {
+
+    //Only change code below this line
+    var myFirstName = "Elvira";
+    var myLastName = "Neziri";
+    var myHomeTown = "Gorazde";
+
+    //Only change code above this line
+
+    return {
+        myFirstName,
+        myLastName,
+        myHomeTown
+    };
+}
+
+console.log(main());
+
+module.exports = main;
